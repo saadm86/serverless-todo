4d13b00633cc34eb77ca1890e4a0c3ec18ad233a
Saad Muhamamd <s.muhammad86@gmail.com>
2021-01-07 07:22:35 +0500

    added netlify auth

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,13 +1,32 @@
-import React, { useEffect } from "react";
-import { Container, Heading, Button, Flex } from "theme-ui";
+import React, { useEffect, useState } from "react";
+import { Container, Heading, Button, Flex, NavLink } from "theme-ui";
 import netlifyIdentity from "netlify-identity-widget";
+import {Link} from 'gatsby'
 
 export default (props) => {
+    
+    const [user, setUser] = useState()
+
   useEffect(() => {
     netlifyIdentity.init({});
+    netlifyIdentity.on("login", user => setUser(user))
+    netlifyIdentity.on("logout", () => setUser())
   });
   return (
     <Container>
+      <Flex as="nav">
+        <NavLink as={Link} to='/' p={2}>
+          Home
+        </NavLink>
+        <NavLink as={Link} to='/app' p={2}>
+          DashBoard
+        </NavLink>
+        {user && (
+            <NavLink href="#!" p={2}>
+                {user.user_metadata.fullname}
+            </NavLink>
+        )}
+      </Flex>
       <Flex sx={{ flexDirection: "column", padding: 3 }}>
         <Heading as="h1">Get Stuff Done</Heading>
         <Button sx={{ marginTop: 2 }} onClick={() => netlifyIdentity.open()}>
