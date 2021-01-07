cc8da8c34e63d57e29d320b226a03c853106908b
Saad Muhamamd <s.muhammad86@gmail.com>
2021-01-07 06:35:19 +0500

    added landing page

---
+++ packages/www/wrap-root-element.js
@@ -0,0 +1,13 @@
+
+const React = require('react');
+const { ThemeProvider } = require('theme-ui');
+const {dark} = require('@theme-ui/presets');
+
+const newTheme = {
+  ...dark,
+  sizes: { container: 1024 }
+};
+
+module.exports = ({ element }) => (
+  <ThemeProvider theme={newTheme}>{element}</ThemeProvider>
+);
